cd4071d0435832d0f795115a8cdd46dd6bd719f4
Matthew Martin <martim01@outlook>
2023-07-27 16:56:45 +0100

    fix: make namedtype.h play nice

--- include/sapnamedtype.h
+++ include/sapnamedtype.h
@@ -1,18 +1,7 @@
 #pragma once
 #include <string>
+#include "namedtype.h"
 
-
-template <typename T, typename Parameter>
-class NamedType
-{
-    public:
-        explicit NamedType(T const& value) : m_value(value){}
-        explicit NamedType(T&& value) : m_value(std::move(value)){}
-        T& Get() {return m_value;}
-        T const& Get() const {return m_value;}
-    private:
-        T m_value;
-};
 
 using IpAddress = NamedType<std::string, struct IpAddressParameter>;
 using IpInterface = NamedType<std::string, struct IpInterfaceParameter>;
